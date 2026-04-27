d30c6311f061ed439d7a7639fe0512e84e6220e4
leoaguinaga <u21219471@utp.edu.pe>
2026-04-27 10:34:10 -0500

    feat: implement custom date picker component and update pricing card data attributes

--- src/core/ProcessSteepData.ts
+++ src/core/ProcessSteepData.ts
@@ -1,3 +1,8 @@
+import SearchIcon from "@/assets/icons/search.svg?url";
+import CodeIcon from "@/assets/icons/code.svg?url";
+import ScrewdriverIcon from "@/assets/icons/screwdriver.svg?url";
+import RocketIcon from "@/assets/icons/rocket.svg?url";
+
 export interface ProcessStepData {
     icon: string;
     title: string;
@@ -6,22 +11,22 @@ export interface ProcessStepData {
 
 export const processData: ProcessStepData[] = [
     {
-        icon: "search.svg",
+        icon: SearchIcon,
         title: "Investigación y análisis",
         description: "Comprendemos las necesidades del cliente y el mercado para definir los requisitos del proyecto."
     },
     {
-        icon: "code.svg",
+        icon: CodeIcon,
         title: "Diseño y prototipado",
         description: "Creamos diseños y prototipos interactivos para visualizar la solución antes del desarrollo."
     },
     {
-        icon: "screwdriver.svg",
+        icon: ScrewdriverIcon,
         title: "Desarrollo ágil",
         description: "Utilizamos metodologías ágiles para desarrollar el software de manera iterativa y colaborativa."
     },
     {
-        icon: "rocket.svg",
+        icon: RocketIcon,
         title: "Lanzamiento y soporte",
         description: "Implementamos la solución y ofrecemos soporte continuo para asegurar su éxito."
     }
